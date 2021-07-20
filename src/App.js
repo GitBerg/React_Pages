6f067a34982cd47d921d5af9a9342c03bc7cbd7c
Gutemberg S Filho <beguinhoboy@hotmail.com>
2021-07-19 23:53:28 -0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+      name: 'Berg',
+      counter: 0
+    };
+
+  handleAClick = (event) =>{
+    event.preventDefault();
+    const{ counter } = this.state;
+    this.setState({counter: counter + 1})
+  }
+
+  handlePClick = () => {
+    const { name } = this.state;
+    this.setState({name:'Gutto'});
+  }
+   
+  render() {
+    const { name, counter } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Gutemberg 
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
